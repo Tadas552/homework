a1dc82ec06f7118a54fe5dc62fabb02ecadf200f
Tadas <tadasdulke5@gmail.com>
2020-02-27 18:42:14 +0200

    Fixed design

--- movie_autocomplete/src/components/NavBar.js
+++ movie_autocomplete/src/components/NavBar.js
@@ -10,36 +10,43 @@ export default class NavBar extends Component {
             movies: [],
             isOpen: false,
         }
-
         this.handleChange = this.handleChange.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
 
     async handleChange(e) {
-        this.setState({searchText : e.target.value})
-        if(e.target.value) {
-            this.setState({movies: await movies(e.target.value)});
-        }else {
-            this.setState({movies: []});
-        }
-        
+        this.setState({searchText : e.target.value});
+        this.setState({movies: await movies(e.target.value)});
     }
+
+    handleFocus = () => this.setState({isOpen:true});
+    handleBlur = () => this.setState({isOpen:false});
+    handleSelect = (title) => this.setState({searchText:title});
 
 
     render() {
         return (
             <nav>
                 <div className="autocomplete">
-                    <input name="search" type="text" className="search_input" value={this.state.searchText} onChange={this.handleChange} placeholder="Enter a movie name" autocomplete="off" autoFocus></input>
-                        {this.state.searchText && <div className="autocomplete-items">
-                            <div className="main_input">
-                                {this.state.searchText}
-                                <p>Enter a movie name</p>
-                            </div>
-                                 {this.state.movies.slice(0,3).map(movie => 
-                                    {return <div>{movie.original_title}<p>{movie.vote_average} Rating, {movie.release_date && movie.release_date.substring(0,4)}</p></div>})}
+                    <div className="input_container">
+                        <div className="search_svg"/>
+                        <input name="search" type="text" className="search_input" 
+                        value={this.state.searchText} onChange={this.handleChange} 
+                        placeholder="Enter a movie name" onFocus={this.handleFocus}
+                        onBlur={this.handleBlur}
+                        autoComplete="off"></input>
+                    </div>
+                        {this.state.isOpen && this.state.searchText.length>2 && <div className="autocomplete-items">
+                                {this.state.movies.map(movie => 
+                                    {return <div onMouseDown={this.handleSelect.bind(this, movie.original_title)} key={movie.id}>{movie.original_title}
+                                        <p>{movie.vote_average} Rating, 
+                                        {movie.release_date && movie.release_date.substring(0,4)}</p>
+                                        </div>})}
                         </div>}
                 </div>
-                <button className="search_button"/>
+                <button className="search_button"></button>
             </nav>
         )
     }
